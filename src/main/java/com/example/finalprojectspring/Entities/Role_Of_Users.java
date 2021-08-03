73ae23105bb07c5bfa7aab43479f702f0da5432b
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-08-04 01:46:32 +0300

    debug

--- src/main/java/com/example/finalprojectspring/Entities/Role_Of_Users.java
+++ src/main/java/com/example/finalprojectspring/Entities/Role_Of_Users.java
@@ -3,9 +3,9 @@ package com.example.finalprojectspring.Entities;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role_Of_Users implements GrantedAuthority {
-    ADMIN,
-    USER,
-    MASTER;
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_MASTER;
 
     @Override
     public String getAuthority() {
--- src/main/java/com/example/finalprojectspring/SecurityConfiguration/SecurityConfig.java
+++ src/main/java/com/example/finalprojectspring/SecurityConfiguration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/main", "/login", "/registration").permitAll()
                 .antMatchers("/admin_page/**").hasRole("ADMIN")
