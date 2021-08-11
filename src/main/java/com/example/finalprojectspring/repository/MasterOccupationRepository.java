a81d8b8faac7b6e4122cef19ff3fd6257bd73779
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-08-11 15:50:04 +0300

    started work with sing up users

--- src/main/java/com/example/finalprojectspring/repository/MasterOccupationRepository.java
+++ src/main/java/com/example/finalprojectspring/repository/MasterOccupationRepository.java
@@ -2,6 +2,8 @@ package com.example.finalprojectspring.repository;
 
 import com.example.finalprojectspring.entities.MasterOcupationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MasterOccupationRepository extends JpaRepository<MasterOcupationEntity, Long> {
 }
